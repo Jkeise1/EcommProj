4414a4d68f703ac2412c55fad76bee9935ddc481
Jkeise1 <you@example.com>
2024-01-22 15:34:50 -0600

    Javascript

--- scripts/login.js
+++ scripts/login.js
@@ -1,22 +1,19 @@
 const apiBaseURL = "http://microbloglite.us-east-2.elasticbeanstalk.com";
 const username = document.getElementById("username");
-const fullName = document.getElementById("fullName");
 const password = document.getElementById("password");
 const formErrorMessage = document.getElementById("formErrorMessage");
 const usernameErrorMessage = document.getElementById("usernameErrorMessage");
-const fullNameErrorMessage = document.getElementById("fullNameErrorMessage");
 const passwordErrorMessage = document.getElementById("passwordErrorMessage");
-const registerButton = document.getElementById("registerButton");
+const loginButton = document.getElementById("loginButton");
 
 function togglePassword() {
     const passwordInput = document.getElementById("password");
     passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 }
 
-function register() {
+function login() {
     formErrorMessage.textContent = "";
     usernameErrorMessage.textContent = "";
-    fullNameErrorMessage.textContent = "";
     passwordErrorMessage.textContent = "";
     if (password.value.length >= 8) {
         fetch(apiBaseURL + "/api/users", {
@@ -24,7 +21,6 @@ function register() {
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
                 username: username.value,
-                fullName: fullName.value,
                 password: password.value
             }),
         }).then(response => {
@@ -33,32 +29,28 @@ function register() {
                 window.location.assign("index.html");
             } else {
                 response.json().then(error => {
-                    handleRegistrationError(error);
+                    handleLoginError(error);
                 });
             }
         });
     } else {
-        handleRegistrationError({ message: "Password must be at least 8 characters long." });
+        handleLoginError({ message: "Password must be at least 8 characters long." });
     }
 }
 
-function handleRegistrationError(error) {
+function handleLoginError(error) {
     username.classList.add("error");
-    fullName.classList.add("error");
     password.classList.add("error");
     username.placeholder = "Enter your username";
-    fullName.placeholder = "Enter your full name";
+
     password.placeholder = "Enter your password";
     if (error.statusCode === 409 && error.message.includes("username")) {
         formErrorMessage.textContent = "Username already taken. Please choose a different one.";
     } else {
-        formErrorMessage.textContent = "Registration failed. Check your input.";
+        formErrorMessage.textContent = "Login failed. Check your input.";
     }
     if (error.message.includes("username")) {
         usernameErrorMessage.textContent = "Username already taken. Please choose a different one.";
-    }
-    if (error.message.includes("fullName")) {
-        fullNameErrorMessage.textContent = "Full name is required.";
     }
     if (error.message.includes("password")) {
         passwordErrorMessage.textContent = "Password is required.";
@@ -69,13 +61,11 @@ function handleRegistrationError(error) {
 
 function resetForm() {
     username.classList.remove("error");
-    fullName.classList.remove("error");
     password.classList.remove("error");
     username.value = "";
-    fullName.value = "";
     password.value = "";
     username.placeholder = "Enter your username";
-    fullName.placeholder = "Enter your full name";
     password.placeholder = "Enter your password";
     formErrorMessage.textContent = "";
 }
+
